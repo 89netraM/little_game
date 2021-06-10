2a6fec681b10b1170820dd8fbefaa8d3b474cc34
Mårten Åsberg <marten.asberg@outlook.com>
2021-06-10 10:13:49 +0200

    Credits include links!

--- src/game/menu_state.rs
+++ src/game/menu_state.rs
@@ -28,6 +28,8 @@ impl InnerGameState for MenuState {
 	fn step(&mut self, window: &mut Window) -> Option<Box<dyn InnerGameState>> {
 		let start_clicked;
 		let exit_clicked;
+		let me_clicked;
+		let game_jam_clicked;
 		{
 			let mut ui = window.conrod_ui_mut().set_widgets();
 
@@ -59,18 +61,48 @@ impl InnerGameState for MenuState {
 				.mid_bottom_with_margin(100.0)
 				.set(self.ui_ids.exit_button, &mut ui);
 
-			widget::Text::new("Created by Mårten Åsberg for the 4MB Game Jam")
+			widget::Text::new("Created by")
 				.font_size(12)
 				.color(Color::Rgba(1.0, 1.0, 1.0, 1.0))
-				.mid_bottom_with_margin(10.0)
-				.center_justify()
-				.set(self.ui_ids.credits, &mut ui);
+				.bottom_left_with_margin(10.0)
+				.set(self.ui_ids.created_text, &mut ui);
+			me_clicked = widget::Button::new()
+				.color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.hover_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.press_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.border_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.label("Mårten Åsberg")
+				.label_font_size(12)
+				.label_color(Color::Rgba(0.0, 0.5, 1.0, 1.0))
+				.right_from(self.ui_ids.created_text, 15.0)
+				.set(self.ui_ids.me_link, &mut ui);
+			widget::Text::new("for the")
+				.font_size(12)
+				.color(Color::Rgba(1.0, 1.0, 1.0, 1.0))
+				.right_from(self.ui_ids.me_link, 17.0)
+				.set(self.ui_ids.for_text, &mut ui);
+			game_jam_clicked = widget::Button::new()
+				.color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.hover_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.press_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.border_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.label("4MB game jam")
+				.label_font_size(12)
+				.label_color(Color::Rgba(0.0, 0.5, 1.0, 1.0))
+				.right_from(self.ui_ids.for_text, 30.0)
+				.set(self.ui_ids.game_jam_link, &mut ui);
 		}
 
 		if start_clicked.was_clicked() {
 			Some(Box::new(super::PlayingState::new(window, 0)))
 		} else if exit_clicked.was_clicked() {
 			window.close();
+			None
+		} else if me_clicked.was_clicked() {
+			let _ = webbrowser::open("https://åsberg.net/");
+			None
+		} else if game_jam_clicked.was_clicked() {
+			let _ = webbrowser::open("https://itch.io/jam/4mb");
 			None
 		} else {
 			None
@@ -83,6 +115,9 @@ widget_ids! {
 		title,
 		start_button,
 		exit_button,
-		credits,
+		created_text,
+		me_link,
+		for_text,
+		game_jam_link,
 	}
 }
--- src/game/pause_state.rs
+++ src/game/pause_state.rs
@@ -30,6 +30,8 @@ impl InnerGameState for PauseState {
 	fn step(&mut self, window: &mut Window) -> Option<Box<dyn InnerGameState>> {
 		let continue_clicked;
 		let menu_clicked;
+		let me_clicked;
+		let game_jam_clicked;
 		{
 			let mut ui = window.conrod_ui_mut().set_widgets();
 
@@ -60,6 +62,37 @@ impl InnerGameState for PauseState {
 				.w(200.0)
 				.mid_bottom_with_margin(100.0)
 				.set(self.ui_ids.menu_button, &mut ui);
+
+			widget::Text::new("Created by")
+				.font_size(12)
+				.color(Color::Rgba(1.0, 1.0, 1.0, 1.0))
+				.bottom_left_with_margin(10.0)
+				.set(self.ui_ids.created_text, &mut ui);
+			me_clicked = widget::Button::new()
+				.color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.hover_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.press_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.border_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.label("Mårten Åsberg")
+				.label_font_size(12)
+				.label_color(Color::Rgba(0.0, 0.5, 1.0, 1.0))
+				.right_from(self.ui_ids.created_text, 15.0)
+				.set(self.ui_ids.me_link, &mut ui);
+			widget::Text::new("for the")
+				.font_size(12)
+				.color(Color::Rgba(1.0, 1.0, 1.0, 1.0))
+				.right_from(self.ui_ids.me_link, 17.0)
+				.set(self.ui_ids.for_text, &mut ui);
+			game_jam_clicked = widget::Button::new()
+				.color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.hover_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.press_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.border_color(Color::Rgba(0.0, 0.0, 0.0, 0.0))
+				.label("4MB game jam")
+				.label_font_size(12)
+				.label_color(Color::Rgba(0.0, 0.5, 1.0, 1.0))
+				.right_from(self.ui_ids.for_text, 30.0)
+				.set(self.ui_ids.game_jam_link, &mut ui);
 		}
 
 		if continue_clicked.was_clicked() {
@@ -69,6 +102,12 @@ impl InnerGameState for PauseState {
 			)))
 		} else if menu_clicked.was_clicked() {
 			Some(Box::new(super::MenuState::new(window)))
+		} else if me_clicked.was_clicked() {
+			let _ = webbrowser::open("https://åsberg.net/");
+			None
+		} else if game_jam_clicked.was_clicked() {
+			let _ = webbrowser::open("https://itch.io/jam/4mb");
+			None
 		} else {
 			None
 		}
@@ -80,5 +119,9 @@ widget_ids! {
 		title,
 		continue_button,
 		menu_button,
+		created_text,
+		me_link,
+		for_text,
+		game_jam_link,
 	}
 }
